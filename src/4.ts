607e13f5dd95ff7809b2fa3c52006e6297ba1fa6
sergeysheva32 <sergeysheva32@gmail.com>
2024-01-08 22:36:27 +0100

    first commit

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,56 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number { return this.signature; }
+}
 
 
-export {};
+class Person {
+  constructor(private key: Key) { }
+
+  getKey(): Key { return this.key }
+}
+
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} ввійшов в будинок.`);
+    } else {
+      console.log("Вхід заборонено.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відкриті.");
+    } else {
+      console.log("Не вдалося відкрити двері.");
+    }
+  }
+}
+
+// Приклад використання класів
+const myKey = new Key();
+
+const myHouse = new MyHouse(myKey);
+const person = new Person(myKey);
+
+myHouse.openDoor(person.getKey());
+
+myHouse.comeIn(person);
+
+export { };
